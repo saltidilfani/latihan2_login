0c4e24398358ee616739c03ed57c85fe30af3ad9
saltidilfani <saltidilfani74@gmail.com>
2024-08-30 15:49:08 +0700

    LATIHAN 2 KOTLIN

---
+++ latihan2_mobileapp_login/app/src/main/java/com/salti/latihan2_mobileapp_login/welcome.kt
@@ -0,0 +1,21 @@
+package com.salti.latihan2_mobileapp_login
+
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
